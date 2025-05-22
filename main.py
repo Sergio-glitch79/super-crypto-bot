18307c20ef05e590cb26833246f25f23072c3e44
Sergio Glitch <shtriny@gmail.com>
2025-05-22 18:53:53 +0300

    Добавлен маршрут / для Flask

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-from flask import Flask, request
-from telegram import Bot, Update
-from telegram.ext import Dispatcher, CommandHandler
-
-TOKEN = '7049684701:AAFoGlDQKQBPg1Tw9Xa3p2btw5IHgCPM8Qg'
-bot = Bot(token=TOKEN)
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    update = Update.de_json(request.get_json(force=True), bot)
-    dispatcher.process_update(update)
-    return 'ok'
-
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Бот работает!")
-
-from telegram.ext import Updater
-updater = Updater(TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler('start', start))
+# Главная страница — просто проверка, что всё работает
+@app.route('/')
+def home():
+    return 'Супербот работает!'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
-
+    app.run(host='0.0.0.0', port=5000)
