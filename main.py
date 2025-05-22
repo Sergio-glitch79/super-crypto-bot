79be6309251f52a61ac8d4c2305661dc0127e5ed
Sergio Glitch <shtriny@gmail.com>
2025-05-22 19:23:13 +0300

    Добавил webhook и обработку /start

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, request
+from telegram import Bot, Update
+from telegram.ext import Dispatcher, CommandHandler
+import os
+
+TOKEN = '7049684701:AAFoGlDQKQBPg1Tw9Xa3p2btw5IHgCPM8Qg'
+bot = Bot(token=TOKEN)
 
 app = Flask(__name__)
 
-# Главная страница — просто проверка, что всё работает
 @app.route('/')
-def home():
+def index():
     return 'Супербот работает!'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    update = Update.de_json(request.get_json(force=True), bot)
+    dispatcher.process_update(update)
+    return 'OK'
+
+def start(update, context):
+    update.message.reply_text('Привет! Я супербот!')
+
+# Подключаем диспетчер
+dispatcher = Dispatcher(bot, None, use_context=True)
+dispatcher.add_handler(CommandHandler('start', start))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
